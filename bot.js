bbe5e4d60da90acfb85d4dbc6ff9a83d72c828c8
Yoshiro00 <98204934+Yoshiro00@users.noreply.github.com>
2023-02-19 16:29:11 +0800

    UpsJS

--- bot.js
+++ bot.js
@@ -13,33 +13,40 @@ async function sendUserMessage() {
 
 const stringSimilarity = require('string-similarity');
 
-async function getChatbotResponse(userMessage) {
+async function getChatbotResponse(userMessage, similarityThreshold = 0.6) {
     try {
-        const response = await fetch('data.json');
-        const data = await response.json();
-
+        const files = ['data1.json', 'data2.json', 'data3.json'];
         let bestMatch = {
             index: -1,
             score: 0,
-            responses: []
+            responses: [],
+            similarPatterns: []
         };
 
-        for (let i = 0; i < data.intents.length; i++) {
-            const intent = data.intents[i];
-            for (let j = 0; j < intent.patterns.length; j++) {
-                const pattern = intent.patterns[j];
-                const similarity = stringSimilarity.compareTwoStrings(userMessage.toLowerCase(), pattern.toLowerCase());
-                if (similarity > bestMatch.score) {
-                    bestMatch.index = i;
-                    bestMatch.score = similarity;
-                    bestMatch.responses = intent.responses;
+        for (const file of files) {
+            const response = await fetch(file);
+            const data = await response.json();
+
+            for (let i = 0; i < data.intents.length; i++) {
+                const intent = data.intents[i];
+                for (let j = 0; j < intent.patterns.length; j++) {
+                    const pattern = intent.patterns[j];
+                    const similarity = stringSimilarity.compareTwoStrings(userMessage.toLowerCase(), pattern.toLowerCase());
+                    if (similarity > bestMatch.score) {
+                        bestMatch.index = i;
+                        bestMatch.score = similarity;
+                        bestMatch.responses = intent.responses;
+                        bestMatch.similarPatterns = [{ pattern, similarity }];
+                    } else if (similarity > similarityThreshold && similarity === bestMatch.score) {
+                        bestMatch.similarPatterns.push({ pattern, similarity });
+                    }
                 }
             }
         }
 
         if (bestMatch.index > -1) {
             const response = bestMatch.responses[Math.floor(Math.random() * bestMatch.responses.length)];
-            return response;
+            return { response, similarPatterns: bestMatch.similarPatterns };
         }
 
         return "I'm sorry, I didn't understand that, I'm still being trained so there might be some words that I don't understand yet.";
