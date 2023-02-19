246ae06165832959410aa4d90df680b09e7527a9
Yoshiro00 <98204934+Yoshiro00@users.noreply.github.com>
2023-02-19 17:21:59 +0800

    DD9

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+const stringSimilarity = require('string-similarity');
+
 async function sendUserMessage() {
     const userMessage = document.getElementById("messageBot").value;
     if (userMessage.trim() === "") {
@@ -6,50 +8,55 @@ async function sendUserMessage() {
 
     const chatbotResponse = await getChatbotResponse(userMessage);
 
-    addBotMessage(chatbotResponse);
+    addBotMessage(chatbotResponse.response);
+    if (chatbotResponse.similarPatterns.length > 0) {
+        console.log("Similar patterns found: ");
+        console.log(chatbotResponse.similarPatterns);
+    }
 
     document.getElementById("messageBot").value = "";
 }
 
-const stringSimilarity = require('string-similarity');
-
-async function getChatbotResponse(userMessage) {
+async function getChatbotResponse(userMessage, similarityThreshold = 0.6) {
     try {
-    const files = ['data1.json', 'data2.json', 'data3.json'];
-    let bestMatch = {
-        index: -1,
-        score: 0,
-        responses: [],
-        similarPatterns: []
-    };
+        const files = ['data1.json', 'data2.json', 'data3.json'];
+        let bestMatch = {
+            index: -1,
+            score: 0,
+            responses: [],
+            similarPatterns: []
+        };
 
-    for (const file of files) {
-        const response = await fetch(file);
-        const data = await response.json();
+        for (const file of files) {
+            const response = await fetch(file);
+            const data = await response.json();
 
-        for (let i = 0; i < data.intents.length; i++) {
-        const intent = data.intents[i];
-            for (let j = 0; j < intent.patterns.length; j++) {
-                const pattern = intent.patterns[j];
-                const similarity = stringSimilarity.compareTwoStrings(userMessage.toLowerCase(), pattern.toLowerCase());
-                if (similarity > bestMatch.score) {
-                    bestMatch.index = i;
-                    bestMatch.score = similarity;
-                    bestMatch.responses = intent.responses;
-                    bestMatch.similarPatterns = [{ pattern, similarity }];
-                } else if (similarity > 0.6 && similarity === bestMatch.score) {
-                    bestMatch.similarPatterns.push({ pattern, similarity });
+            for (let i = 0; i < data.intents.length; i++) {
+                const intent = data.intents[i];
+                for (let j = 0; j < intent.patterns.length; j++) {
+                    const pattern = intent.patterns[j];
+                    const similarity = stringSimilarity.compareTwoStrings(userMessage.toLowerCase(), pattern.toLowerCase());
+                    if (similarity > bestMatch.score) {
+                        bestMatch.index = i;
+                        bestMatch.score = similarity;
+                        bestMatch.responses = intent.responses;
+                        bestMatch.similarPatterns = [{ pattern, similarity }];
+                    } else if (similarity > similarityThreshold && similarity === bestMatch.score) {
+                        bestMatch.similarPatterns.push({ pattern, similarity });
+                    }
                 }
             }
         }
-    }
 
-    if (bestMatch.index > -1) {
-        const response = bestMatch.responses[Math.floor(Math.random() * bestMatch.responses.length)];
-        return response;
-    }
+        if (bestMatch.index > -1) {
+            const response = bestMatch.responses[Math.floor(Math.random() * bestMatch.responses.length)];
+            return { response, similarPatterns: bestMatch.similarPatterns };
+        }
 
-    return "I'm sorry, I didn't understand that, I'm still being trained so there might be some words that I don't understand yet.";
+        return {
+            response: "I'm sorry, I didn't understand that, I'm still being trained so there might be some words that I don't understand yet.",
+            similarPatterns: []
+        };
     } catch (error) {
         console.error(error);
     }
