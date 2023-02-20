b64aacee5b2fc37a561f156ce16f8ba5334082f0
Yoshiro00 <98204934+Yoshiro00@users.noreply.github.com>
2023-02-20 17:28:51 +0800

    GG!11

--- bot.js
+++ bot.js
@@ -1,61 +1,35 @@
-async function sendMessage() {
+async function sendUserMessage() {
+    // Get user's message from input box
     const userMessage = document.getElementById("messageBot").value;
     if (userMessage.trim() === "") {
         return;
     }
 
+    // Send user's message to chatbot and wait for response
     const chatbotResponse = await getChatbotResponse(userMessage);
 
-    addBotMessage(chatbotResponse.response);
-    if (chatbotResponse.similarPatterns.length > 0) {
-        console.log("Similar patterns found: ");
-        console.log(chatbotResponse.similarPatterns);
-    }
+    // Display chatbot's response in the chat window
+    addBotMessage(chatbotResponse);
 
+    // Clear user's message from input box
     document.getElementById("messageBot").value = "";
 }
 
-window.sendMessage = sendMessage;
-
-async function getChatbotResponse(userMessage, similarityThreshold = 0.6) {
-    const stringSimilarity = require('string-similarity');
-    
+async function getChatbotResponse(userMessage) {
     try {
-        const response = await fetch('data1.json');
+        const response = await fetch('data.json');
         const data = await response.json();
-
-        let bestMatch = {
-            index: -1,
-            score: 0,
-            responses: [],
-            similarPatterns: []
-        };
-
         for (let i = 0; i < data.intents.length; i++) {
             const intent = data.intents[i];
             for (let j = 0; j < intent.patterns.length; j++) {
                 const pattern = intent.patterns[j];
-                const similarity = stringSimilarity.compareTwoStrings(userMessage.toLowerCase(), pattern.toLowerCase());
-                if (similarity > bestMatch.score) {
-                    bestMatch.index = i;
-                    bestMatch.score = similarity;
-                    bestMatch.responses = intent.responses;
-                    bestMatch.similarPatterns = [{ pattern, similarity }];
-                } else if (similarity > similarityThreshold && similarity === bestMatch.score) {
-                    bestMatch.similarPatterns.push({ pattern, similarity });
+                if (userMessage.toLowerCase().includes(pattern.toLowerCase())) {
+                    const response = intent.responses[Math.floor(Math.random() * intent.responses.length)];
+                    return response;
                 }
             }
         }
-
-        if (bestMatch.index > -1) {
-            const response = bestMatch.responses[Math.floor(Math.random() * bestMatch.responses.length)];
-            return { response, similarPatterns: bestMatch.similarPatterns };
-        }
-
-        return {
-            response: "I'm sorry, I didn't understand that, I'm still being trained so there might be some words that I don't understand yet.",
-            similarPatterns: []
-        };
+        return "I'm sorry, I didn't understand that, I'm still being trained so there might be some words that I don't understand yet.";
     } catch (error) {
         console.error(error);
     }
